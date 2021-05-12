f195740d10897273ee36757ad22eb25b779c58a6
AndrzejSkaza <72215327+AndrzejSkaza@users.noreply.github.com>
2021-05-12 20:01:20 +0200

    [add] first rest controller

--- src/main/java/pl/sda/mybudget/model/Income.java
+++ src/main/java/pl/sda/mybudget/model/Income.java
@@ -1,4 +1,19 @@
 package pl.sda.mybudget.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import pl.sda.mybudget.model.enumeration.IncomeType;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
 public class Income {
+
+    private long amountInPolishGrosz;
+     private LocalDate incomeDate;
+     private String incomeSources;
+     private IncomeType incomeType;
+
 }
